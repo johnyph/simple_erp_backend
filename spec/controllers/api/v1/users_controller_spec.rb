5e7238d6c7c0f0e30c3fbe8434e5b4c149f7b8a0
Ioannis-Andreas Philippas <iphilippas@gmail.com>
2015-01-22 13:41:37 +0200

    create users

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -16,4 +16,34 @@ RSpec.describe Api::V1::UsersController, :type => :controller do
 
     it { should respond_with 200 }
   end
+
+  describe 'POST #create' do
+    context 'when successfully created' do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it 'renders the created user in json' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email] 
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context 'when is not created' do
+      before(:each) do
+        @invalid_user_attributes = { password: '12345678', password_confirmation: '12345678' }
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+
+      it 'renders an error json' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it { should respond_with 422 }
+    end
+  end
 end
