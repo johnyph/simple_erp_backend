a1ac2ad7c7a36c13d8bdcac039eeea27c79be132
Ioannis-Andreas Philippas <iphilippas@gmail.com>
2015-01-27 17:13:43 +0200

    change delete dependancy of organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,7 +3,7 @@ class Organization < ActiveRecord::Base
 	validates :afm, uniqueness: true
   validates :afm, length: {minimum: 9, maximum: 9}
 
-  has_many :users, dependent: :delete_all
-  has_many :customers, dependent: :delete_all
-  has_many :suppliers, dependent: :delete_all
+  has_many :users, dependent: :destroy
+  has_many :customers, dependent: :destroy
+  has_many :suppliers, dependent: :destroy
 end
